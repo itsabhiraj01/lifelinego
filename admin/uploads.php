e81ef0b858267779aec581d1052e3b0b1499f854
Abhiraj Singh Tomar <abhirajsinghtomar@gmail.com>
2017-08-01 11:31:08 +0530

    updated till lec 76

--- admin/uploads.php
+++ admin/uploads.php
@@ -4,13 +4,18 @@
 
  $storeFolder = '../uploads'; //2
 
+$ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+$newname = time();
+$random = rand(100,999);
+$name = $newname.$random.'.'.$ext;
+
  if(!empty($_FILES)) {
 
  	$tempFile = $_FILES['file']['tmp_name']; //3
 
  	$targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds; //4
 
- 	$targetFile = $targetPath. $_FILES['file']['name']; //5
+ 	$targetFile = $targetPath. $name; //5
 
  	move_uploaded_file($tempFile,$targetFile); //6
 
